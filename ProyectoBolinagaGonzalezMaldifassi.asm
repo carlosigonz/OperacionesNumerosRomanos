68502bed7aaeae92ad96e1e731d8bdc8247e4775
Carlos Gonzalez <carlosigonz@gmail.com>
2021-03-11 16:22:14 -0400

    Agregado mensajes

--- ProyectoBolinagaGonzalezMaldifassi.asm
+++ ProyectoBolinagaGonzalezMaldifassi.asm
@@ -1,12 +1,33 @@
 .data
-	saludo: .asciiz  "Ingrese un numero Romano: \n"
-	numero: .space 10
-	prueba: .asciiz  "El numero es: "
+	separador: .asciiz "\n-------------------------------------------------------------------------------------------------------------------\n"
+	saludo: .asciiz  "Bienvenido a operaciones algebraicas con números romanos!\n"
+	menu: .asciiz "Seleccione: \n1.Suma\n2.Resta\n3.Multiplicación\n"
+	entrada1: .asciiz "Ingrese el primer número Romano: \n"
+	entrada2: .asciiz "Ingrese el segundo número Romano: \n"
+	numero1: .space 10
+	numero2: .space 10
+	msj1: .asciiz "El primer número es: "
+	msj2: .asciiz "El segundo número es: "
 .text
 	#imprime saludo
 	li $v0,4
 	la $a0, saludo
-	syscall 
+	syscall
+	
+	#imprime separacion
+	li $v0,4
+	la $a0, separador
+	syscall
+	
+	#imprime menu
+	li $v0,4
+	la $a0, menu
+	syscall
+	
+	#imprime mensaje de entrada
+	li $v0,4
+	la $a0, entrada
+	syscall
 	
 	#toma el numero romano
 	li $v0,8
@@ -14,10 +35,13 @@
 	li $a1,10
 	syscall
 	
-	#imprime mensaje de despedida
 	li $v0,4
-	la $a0, prueba
-	syscall 
+	la $a0,msj1
+	syscall
+	#imprime mensaje de despedida
+	#li $v0,4
+	#la $a0, num1
+	#syscall 
 	
 	#imprime el numero insertado
 	li $v0,4
