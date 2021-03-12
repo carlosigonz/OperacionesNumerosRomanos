86ed1af5d7ed9add6eb4b9743a9e07f6a3c00c37
Carlos Gonzalez <carlosigonz@gmail.com>
2021-03-12 11:53:51 -0400

    Primer intento de loop

--- ProyectoBolinagaGonzalezMaldifassi.asm
+++ ProyectoBolinagaGonzalezMaldifassi.asm
@@ -10,6 +10,7 @@
 	numero1: .space 15
 	numero2: .space 15
 	msj1: .asciiz "El primer número (A) es: "
+	long1: .asciiz "Longitud pri-num: "
 	msj2: .asciiz "El segundo número (B) es: "
 	resultado: .asciiz "El resultado de la operación es: "
 	
@@ -23,15 +24,18 @@
 	li $v0,4
 	la $a0,%label
 	syscall
-	.end_macro 
+	.end_macro
+
 .text
 	#imprime saludo
 	printMsj(saludo)
 	
 	separacion
+	
 	#imprime mensaje de entrada pri-num
 	printMsj(entrada1)
-		#toma el pri-num
+	
+	#toma el pri-num
 	li $v0,8
 	la $a0, numero1
 	li $a1,15
@@ -61,11 +65,24 @@
 	printMsj(numero2)
 	
 	#loop iterativo
-	li $t0,0
-	li $t1,0
-	li $t2,0
-	#len1: 
-	#	lb $t3,numero1($t0)
+	li $t0,0 #iterador
+	#li $t1,0
+	#li $t2,0
+	la $t0,numero1
+	len1: 
+		lb $t2 0($t0)
+		beq $t2 $zero finLen1
+		addi $t0,$t0,1
+		j len1
+	finLen1:
+		la $t2, numero1
+		sub $t1,$t0,$t2 #$t1 ahora tiene la longitud del primer numero
+		
+	printMsj(long1)
+	
+	li $v0,1
+	addi $a0,$t1,-1
+	syscall
 	
 	#imprime menu
 	printMsj(menu)
@@ -77,6 +94,7 @@
 	beq $v0,1,suma
 	beq $v0,2,resta
 	beq $v0,3,multi
+	
 	
 	suma:
 		#imprime msj
